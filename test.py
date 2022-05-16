3d77483bb0c68e2db2b40b14657a0893caea109e
Ikuya Fujikawa <tsukutakun@icloud.com>
2022-05-16 15:09:59 +0900

    solving error

---
+++ test.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+df = pd.read_csv("data/student-mat.csv",sep=";")
+plt.hist(df['absences'])
+
+plt.xlabel('absences')
+plt.ylabel('count')
+
+plt.grid(True)
